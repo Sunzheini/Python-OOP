d7450461527fc812e27a7ec8fe16f022d4255bfd
Daniel Zorov <daniel_zorov@abv.bg>
2022-08-06 17:14:30 +0300

    Update060822

---
+++ lab/l010822_Design_Patterns.py
@@ -0,0 +1,26 @@
+# Design Patterns
+
+
+# design patterns - reshavat problemi i davat abstrakciq vyrhu samoto reshenie
+# creational, structural, behavioral
+
+# -------------------------------------------------------------------
+# Creational - abstrakciq pri syzdavane na obekti
+
+
+# -------------------------------------------------------------------
+# Structural - abstrakciq vyrhu strukturata
+
+
+# -------------------------------------------------------------------
+# Behavioral - kak si vzaimodeistvat komponentite
+
+
+
+
+
+
+
+
+
+
