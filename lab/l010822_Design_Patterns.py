5d000d9badc6a33c1a0f11a46237c7dbfbaeddab
Daniel Zorov <daniel_zorov@abv.bg>
2022-08-06 17:45:15 +0300

    Update l010822_Design_Patterns.py

--- lab/l010822_Design_Patterns.py
+++ lab/l010822_Design_Patterns.py
@@ -1,19 +1,39 @@
 # Design Patterns
 
 
-# design patterns - reshavat problemi i davat abstrakciq vyrhu samoto reshenie
+# design patterns - shabloni za dizain
+# reshavat problemi i davat abstrakciq vyrhu samoto reshenie
 # creational, structural, behavioral
 
 # -------------------------------------------------------------------
 # Creational - abstrakciq pri syzdavane na obekti
+import abc
+
+
+class Animal(abc.ABC):
+    @abc.abstractmethod
+    def make_noise(self):
+        pass
+
+
+class Cat(Animal):
+    def __init__(self, name, age):
+        pass
+
+
+class Dog(Animal):
+    def __init__(self, name):
+        pass
+
+# 2:28:43
 
 
 # -------------------------------------------------------------------
-# Structural - abstrakciq vyrhu strukturata
+# Structural - abstrakciq vyrhu strukturata, kak razlichnite komponenti mogat da se strukturirat taka che namalin tqhnoto strukturno vzaimodeistvie
 
 
 # -------------------------------------------------------------------
-# Behavioral - kak si vzaimodeistvat komponentite
+# Behavioral - kak si vzaimodeistvat komponentite, komunikaciqta mejdu tqh
 
 
 
